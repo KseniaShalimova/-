e431994ee2721b2e96438dfc1b621b0e29bb6d33
Ksenia Shalimova <shalimovakss@xmail.ru>
2024-01-20 00:02:18 +0500

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿bool Check(string input)
+{
+    if (input == "q")
+    {
+        return true;
+    }
+    int value = Convert.ToInt32(input);
+    int sum = 0;
+    while (value > 0)
+    {
+        sum = sum + value % 10;
+        value = value / 10;
+    }
+    if (sum % 2 == 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+
+bool EndorNot = true;
+while (EndorNot == true)
+{
+    Console.WriteLine("Введите число ");
+    string input = Console.ReadLine();
+    bool check = Check(input);
+    if (check == true)
+    {
+        EndorNot = false;
+    }
+}
