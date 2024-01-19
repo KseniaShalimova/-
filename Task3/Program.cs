eedfc2f8cbfa3b47307a1ff8f50ba2565e2a7ead
Ksenia Shalimova <shalimovakss@xmail.ru>
2024-01-20 00:53:28 +0500

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] ArrayReversal(int[] arr)
+{
+    for (int i = 0; i < arr.Length / 2; i++)
+    {
+        int number1 = arr[i];
+        arr[i] = arr[arr.Length - (i + 1)];
+        arr[arr.Length - (i + 1)] = number1;
+    }
+    return arr;
+}
+
+
+
+int[] arr = { 1, 2, 3, 4, 5, 6 };
+int[] reversArr = ArrayReversal(arr);
+foreach (int e in reversArr)
+{
+    Console.Write($"{e} ");
+}
